d0d2f922f3792399c1557e99b1b78e262bfa016d
Jed Watson <jed.watson@me.com>
2014-02-25 17:39:12 +1100

    Displaying simple list of train stations in NSW

--- routes/views/stations.js
+++ routes/views/stations.js
@@ -1,5 +1,7 @@
 var keystone = require('keystone');
 
+var Station = keystone.list('Station');
+
 exports = module.exports = function(req, res) {
 	
 	var locals = res.locals,
@@ -7,6 +9,9 @@ exports = module.exports = function(req, res) {
 	
 	// Set locals
 	locals.section = 'stations';
+	
+	// Load the list of stations
+	view.query('stations', Station.model.find().sort('name'));
 	
 	// Render the view
 	view.render('stations');
